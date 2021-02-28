74cc2cb82ccdc6ca7079c7f2d75780ca15d42006
JCodeX-Dev <jcodex.dev@gmail.com>
2021-02-28 18:23:34 +0530

    Added window scroll event for navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  menuItems = [
+    {linkId: 1, linkName: 'Home', linkUrl: '#home'},
+    {linkId: 2, linkName: 'About', linkUrl: '#about'},
+    {linkId: 3, linkName: 'Projects', linkUrl: '#projects'},
+    {linkId: 4, linkName: 'Blogs', linkUrl: '#blogs'},
+    {linkId: 5, linkName: 'Contact', linkUrl: '#contact'}
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  // let selectHeader = select('#header')
+  // if (selectHeader) {
+  //   const headerScrolled = () => {
+  //     if (window.scrollY > 100) {
+  //       selectHeader.classList.add('header-scrolled')
+  //     } else {
+  //       selectHeader.classList.remove('header-scrolled')
+  //     }
+  //   }
+  //   window.addEventListener('load', headerScrolled)
+  //   onscroll(document, headerScrolled)
+  // }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.pageYOffset;
+    if (offset > 100){
+      document.querySelector('header').classList.add('nv-sc');
+    }
+    else {
+      document.querySelector('header').classList.remove('nv-sc');
+    }
+      }
 
 }
