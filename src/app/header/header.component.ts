9fe89b1aa17b66971f6ede5aa3c540827f471b13
JCodeX-Dev <jcodex.dev@gmail.com>
2021-02-28 20:44:02 +0530

    Rearranged code

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -21,20 +21,8 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // let selectHeader = select('#header')
-  // if (selectHeader) {
-  //   const headerScrolled = () => {
-  //     if (window.scrollY > 100) {
-  //       selectHeader.classList.add('header-scrolled')
-  //     } else {
-  //       selectHeader.classList.remove('header-scrolled')
-  //     }
-  //   }
-  //   window.addEventListener('load', headerScrolled)
-  //   onscroll(document, headerScrolled)
-  // }
-
   @HostListener('window:scroll', [])
+  // tslint:disable-next-line:typedef
   onWindowScroll() {
     const offset = window.pageYOffset;
     if (offset > 100){
