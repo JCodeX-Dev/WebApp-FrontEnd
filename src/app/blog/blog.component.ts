86e9674ffcc135fffcb7bdfd535749daa13031c9
JCodeX-Dev <jcodex.dev@gmail.com>
2021-05-31 17:30:59 +0530

    Added Blog Script

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -9,6 +9,19 @@ export class BlogComponent implements OnInit {
 
   constructor() { }
 
+  // tslint:disable-next-line:typedef
+  modalShow(modalView){
+      modalView.classList.add('active-modal');
+  }
+
+  // tslint:disable-next-line:typedef
+  modalClose(){
+    const modalViews = document.querySelectorAll('.blog_modal');
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal');
+    });
+  }
+
   ngOnInit(): void {
   }
 
