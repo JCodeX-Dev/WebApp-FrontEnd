b12545d5cf741fa63f305dd7dba7d5fa8a0238cf
JCodeX-Dev <jcodex.dev@gmail.com>
2021-05-31 13:35:29 +0530

    Added About page Script & Updated HTML page

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-skill',
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  // tslint:disable-next-line:typedef
+  toggleSkills(caller) {
+    const skillsContent = document.getElementsByClassName('skills_content');
+    const itemClass = caller.parentNode.className;
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < skillsContent.length; i++) {
+      skillsContent[i].className = 'skills_content skills_close';
+    }
+
+    if (itemClass === 'skills_content skills_close') {
+      caller.parentNode.className = 'skills_content skills_open';
+    }
+  }
+
 
   ngOnInit(): void {
   }
