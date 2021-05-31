c443f801c824977f24afcd7246bb668d15612aa6
JCodeX-Dev <jcodex.dev@gmail.com>
2021-05-31 15:44:47 +0530

    Added Qualification Script

--- src/app/qualification/qualification.component.ts
+++ src/app/qualification/qualification.component.ts
@@ -9,6 +9,20 @@ export class QualificationComponent implements OnInit {
 
   constructor() { }
 
+  // tslint:disable-next-line:typedef
+  toggleTab(tab, tabc){
+    const tabs = document.querySelectorAll('.qualification_button');
+    const tabContents = document.querySelectorAll('[data-content]');
+    tabContents.forEach(tabContent => {
+      tabContent.classList.add('qualification_inactive');
+    });
+    tabs.forEach(t => {
+      t.classList.add('qualification_inactive');
+    });
+    tab.classList.remove('qualification_inactive');
+    tabc.classList.remove('qualification_inactive');
+  }
+
   ngOnInit(): void {
   }
 
