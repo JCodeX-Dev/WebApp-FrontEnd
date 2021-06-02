23c72005f0e382359043203987102aa1b1c9c1ad
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-02 19:01:57 +0530

    Added Project Script

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Swiper, Navigation, Pagination} from 'swiper/core';
+
+Swiper.use([Navigation, Pagination]);
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements AfterViewInit {
+  swiper: Swiper;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngAfterViewInit(): void {
+    this.swiper = new Swiper('.project_container', {
+      cssMode: true,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+    }
 }
