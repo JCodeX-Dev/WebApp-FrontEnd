ba9f1a19dbf8705c401c3461d110770ce39343b0
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-04 21:41:36 +0530

    updated active nav link script

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,35 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {AfterViewInit, Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
   constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    // const sections = document.querySelectorAll('section[id]');
+    //
+    // // tslint:disable-next-line:typedef
+    // function scrollActive(){
+    //   const scrollY = window.pageYOffset;
+    //
+    //   sections.forEach(current => {
+    //     const sectionHeight = current.scrollHeight;
+    //     const sectionTop = current.scrollTop - 50;
+    //     let sectionId = current.getAttribute('id');
+    //
+    //     if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+    //       document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+    //     }else{
+    //       document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
+    //     }
+    //   });
+    // }
+    // window.addEventListener('scroll', scrollActive);
   }
 
   // menuItems = [
@@ -21,59 +43,51 @@ export class HeaderComponent implements OnInit {
 
 
   /*===== MENU SHOW =====*/
+
   // tslint:disable-next-line:typedef
   showNav() {
     document.getElementById('nav-menu').classList.add('show-menu');
   }
 
   /*===== MENU HIDDEN =====*/
+
   // tslint:disable-next-line:typedef
-  closeNav(){
+  closeNav() {
     document.getElementById('nav-menu').classList.remove('show-menu');
   }
 
   /*==================== REMOVE MENU MOBILE ====================*/
   @HostListener('click', [])
   // tslint:disable-next-line:typedef
-  onClick(){
+  onClick() {
     const navLink = document.querySelectorAll('.nav_link');
+
     // tslint:disable-next-line:typedef
-    function linkAction(){
+    function linkAction() {
       const navMenu = document.getElementById('nav-menu');
       // When we click on each nav__link, we remove the show-menu class
       navMenu.classList.remove('show-menu');
     }
+
     navLink.forEach(n => n.addEventListener('click', linkAction));
   }
 
   @HostListener('window:scroll', [])
   // tslint:disable-next-line:typedef
   onWindowScroll() {
+    const offset = window.scrollY;
     const sections = document.querySelectorAll('section[id]');
-    const offset = window.pageYOffset;
-
     sections.forEach(current => {
-      const sectionHeight = current.clientHeight;
-      const sectionTop = current.scrollTop;
-      const sectionId = current.getAttribute('id') ;
-      // console.log(sectionId);
-      console.log(sectionId + ' ' + offset + ' ' + sectionTop + ' ' + sectionHeight);
-      if (offset > sectionTop && offset <= (sectionTop + sectionHeight)){
+      const section = document.getElementById(current.getAttribute('id'));
+      const sectionHeight = section.offsetHeight;
+      const sectionTop = section.offsetTop - 50;
+      const sectionId = current.getAttribute('id');
+
+      if (offset > sectionTop && offset <= (sectionTop + sectionHeight)) {
         document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
-        console.log(true);
       } else {
         document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
-        console.log(false);
       }
     });
-  //   if (offset > 100) {
-  //     document.querySelector('header').classList.add('nv-sc');
-  //     document.querySelector(':root').setAttribute('style','--nav-color: #fff');
-  //   } else {
-  //     document.querySelector('header').classList.remove('nv-sc');
-  //     document.querySelector(':root').setAttribute('style','--nav-color: #035a6d');
-  //   }
-  }
-  ngOnInit(): void {
   }
 }
