8ce7bd5b288651c1ea23996df6cae8174d021862
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-06 15:51:12 +0530

    dv2is2b1 bug resolved: Added additional assertion statement to stop from making recursive calls

--- src/app/tooltip.directive.ts
+++ src/app/tooltip.directive.ts
@@ -7,6 +7,7 @@ export class TooltipDirective {
   tooltip: HTMLElement;
   // tslint:disable-next-line:no-input-rename
   @Input() appTooltip = '';
+
   // delay = 500;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
@@ -14,10 +15,9 @@ export class TooltipDirective {
 
   // tslint:disable-next-line:typedef
   @HostListener('mouseover') onMouseEnter() {
-
-    // console.log(this.el.nativeElement.tagName);
-    // console.log(this.appTooltip);
-    this.showTooltip();
+    if (this.el.nativeElement.getElementsByTagName('span').length === 0) {
+      this.showTooltip();
+    }
   }
 
   // tslint:disable-next-line:typedef
@@ -30,25 +30,12 @@ export class TooltipDirective {
     // this.tooltip = this.el.nativeElement;
     this.tooltip = this.renderer.createElement('span');
     // creating a span
-    // this.tooltip.appendChild(this.renderer.createElement('span'));
-    // appending a span to the tooltip
 
     this.renderer.appendChild(
       this.tooltip,
       this.renderer.createText(this.appTooltip)
       // adding the tooltip text into the tooltip span
     );
-    // const hostPos = this.el.nativeElement.getBoundingClientRect();
-    // getting the hight width and positions of the target element
-    // tslint:disable-next-line:one-variable-per-declaration
-    // let top, left;
-
-    // top = hostPos.bottom;
-    // left = hostPos.left + hostPos.width / 2;
-    // this.renderer.setStyle(this.tooltip, 'top', `${top}px`);
-    // adding a top positions value for the tooltip
-    // this.renderer.setStyle(this.tooltip, 'left', `${left}px`);
-    // adding the left value
     this.renderer.appendChild(this.el.nativeElement, this.tooltip);
     // appending to the document
     this.renderer.addClass(this.tooltip, 'tooltip');
