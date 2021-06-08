324ce3ec142cb2ab33e44ca6168d05dfbe204876
JCodeX-Dev <jcodex.dev@gmail.com>
2021-06-08 12:51:15 +0530

    Added API details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
-
+export const baseURL = 'localhost:8080/api/profile/';
+export const profile = '1';
+export const apiResource = {
+  header: '/header',
+  home: '/home',
+  about: '/about',
+  skills: '/skills',
+  qualifications: '/qualifications',
+  projects: '/projects',
+  blogs: '/blogs',
+  contact: '/contact',
+  footer: '/footer'
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -14,3 +26,15 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+// api/profile/1/header
+// api/profile/1/home
+// api/profile/1/about
+// api/profile/1/skills
+// api/profile/1/qualifications
+// api/profile/1/projects
+// api/profile/1/blogs
+// api/profile/1/contact
+// api/profile/1/footer
+
